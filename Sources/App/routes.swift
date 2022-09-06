f3f893e247f9d77f83a46c44baae67aea0e3349b
elaz <elaz>
2022-09-06 10:56:19 +0300

    use uuids

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -1,9 +1,18 @@
 import Vapor
 
+let seriesMask:[String: String] = {
+    var dict = [String: String]()
+    for index in 0..<28 {
+        dict[UUID().uuidString] = "\(index)"
+    }
+    return dict
+}()
+
 struct Series: Codable, Content {
     var id = UUID().uuidString
-    var slices = (0..<28).map {"\($0)"}
+    var slices = Array(seriesMask.keys)
 }
+
 
 let series = Series()
 
@@ -19,11 +28,11 @@ func routes(_ app: Application) throws {
             throw Abort(.notFound, reason: "Series not found for given id")
         }
         guard let index = request.parameters.get("index"),
-              let indexVal = Int(index), (0..<28).contains(indexVal) else {
+              seriesMask.keys.contains(index) else {
                   throw Abort(.notFound, reason: "Slice not found for given id")
               }
         
-        let response = request.fileio.streamFile(at: "Public/images/image_\(index).jpg")
+        let response = request.fileio.streamFile(at: "Public/images/image_\(seriesMask[index]).jpg")
         return response
     }
 
